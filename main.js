7ae547a34da09b0c63af0d0085a7af9528e5b1ab
samantha <lattechoi2@gmail.com>
2021-04-05 19:11:42 +0900

    CSS: 필드 opacity 수정 / JS: rect 사이즈 조정

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const CARROT_SIZE = 80;
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect(); 
 
@@ -12,8 +13,8 @@ function initGame() {
 function addItem(className, count, imgPath) {
     const x1 = 0;
     const y1 = 0;
-    const x2 = fieldRect.width;
-    const y2 = fieldRect.height;
+    const x2 = fieldRect.width - CARROT_SIZE;
+    const y2 = fieldRect.height - CARROT_SIZE;
 
     for(let i = 0; i < count; i++) {
         const item = document.createElement('img');
