b8fa202bbb89888e9d9a5d75264c15294a13690f
samantha <lattechoi2@gmail.com>
2021-04-06 00:54:07 +0900

    JS: gameBtn 수정

--- main.js
+++ main.js
@@ -79,6 +79,7 @@ function showStopButton() {
     const icon = gameBtn.querySelector('.fas');
     icon.classList.add('fa-stop');
     icon.classList.remove('fa-play');
+    gameBtn.style.visibility = 'visible';
 }
 
 function hideGameButton() {
